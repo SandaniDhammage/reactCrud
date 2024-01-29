3dbb42e6fa5277ea90a6b61be57001cd25aa83b1
Sandani <sandanid01@gmail.com>
2024-01-29 18:55:38 +0530

    first commit

---
+++ src/components/Employees.js
@@ -0,0 +1,16 @@
+const Employees=[
+    {
+        id:"1",
+        Name:"Sandani Dhammage",
+        Age:"23"
+    },{
+        id:"2",
+        Name:"Hiruni Gamage",
+        Age:"23"
+    },{
+        id:"2",
+        Name:"Tharushi vithana",
+        Age:"23"
+    }
+]
+export default Employees;
